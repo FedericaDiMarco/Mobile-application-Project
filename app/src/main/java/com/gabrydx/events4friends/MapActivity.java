b2b17b877b84d378488947c58ad3bc214e3ff7a4
federica <dimarco.1631365@studenti.uniroma1.it>
2020-04-28 19:05:49 +0200

    Google play dependency and MapActivity.java code

--- app/src/main/java/com/gabrydx/events4friends/MapActivity.java
+++ app/src/main/java/com/gabrydx/events4friends/MapActivity.java
@@ -1,13 +1,39 @@
 package com.gabrydx.events4friends;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MapActivity extends AppCompatActivity {
+/*public class MapActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+    }
+}*/
+public class MapActivity extends AppCompatActivity {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng firenze = new LatLng(43.776366, 11.247822);
+        mMap.addMarker(new MarkerOptions().position(firenze).title("Siamo a Firenze!"));
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(firenze).zoom(15).build();
+
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 }
