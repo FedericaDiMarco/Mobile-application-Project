132325fe43c80f44d58a10cc77dd5b4b8784d4ee
Gabriele Nicosanti <gabrydx@hotmail.com>
2020-04-28 16:43:34 +0200

    Creazione attività mappa

--- app/src/main/java/com/gabrydx/events4friends/MainActivity.java
+++ app/src/main/java/com/gabrydx/events4friends/MainActivity.java
@@ -1,13 +1,30 @@
 package com.gabrydx.events4friends;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button buttonMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonMap = (Button) findViewById(R.id.buttonMap);
+        buttonMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMapActivity();
+            }
+        });
+    }
+
+    public void openMapActivity() {
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
     }
 }
